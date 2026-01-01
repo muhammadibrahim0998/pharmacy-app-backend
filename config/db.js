9b81f5447603ac33aa79129a9683d61b72a7bf74
developer-kpit <ibrahim1530388@gmail.com>
2026-01-01 12:30:45 +0500

    added port to connection

--- config/db.js
+++ config/db.js
@@ -1,21 +1,3 @@
-// import { Sequelize } from "sequelize";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//   }
-// );
-
-// export default sequelize;
-
-
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
@@ -27,16 +9,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+port: process.env.DB_PORT,
     dialect: "mysql",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    logging: false,
   }
 );
 
 export default sequelize;
+
